9415de0850e61b7a176d967fe77e2eaa907ddded
phuongps02360 <phuonghn1805@gmail.com>
2015-04-14 21:37:11 +0700

    ps02360

---
+++ asm/QLsanpham/qlsanpham.vb
@@ -0,0 +1,6 @@
+﻿Public Class qlsanpham
+
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs)
+
+    End Sub
+End Class
---
+++ asm/asm/qlsp.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_qlsp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txt_mlsp = New System.Windows.Forms.TextBox()
+        Me.txt_ctsp = New System.Windows.Forms.TextBox()
+        Me.txt_sl = New System.Windows.Forms.TextBox()
+        Me.txt_dg = New System.Windows.Forms.TextBox()
+        Me.txt_msp = New System.Windows.Forms.TextBox()
+        Me.txt_tsp = New System.Windows.Forms.TextBox()
+        Me.btn_thoat = New System.Windows.Forms.Button()
+        Me.btn_xoa = New System.Windows.Forms.Button()
+        Me.btn_sua = New System.Windows.Forms.Button()
+        Me.btn_them = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.dgv_dssp = New System.Windows.Forms.DataGridView()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.dgv_dssp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Controls.Add(Me.txt_mlsp)
+        Me.GroupBox1.Controls.Add(Me.txt_ctsp)
+        Me.GroupBox1.Controls.Add(Me.txt_sl)
+        Me.GroupBox1.Controls.Add(Me.txt_dg)
+        Me.GroupBox1.Controls.Add(Me.txt_msp)
+        Me.GroupBox1.Controls.Add(Me.txt_tsp)
+        Me.GroupBox1.Controls.Add(Me.btn_thoat)
+        Me.GroupBox1.Controls.Add(Me.btn_xoa)
+        Me.GroupBox1.Controls.Add(Me.btn_sua)
+        Me.GroupBox1.Controls.Add(Me.btn_them)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 14)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(600, 274)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Thông tin sản phẩm"
+        '
+        'txt_mlsp
+        '
+        Me.txt_mlsp.Location = New System.Drawing.Point(131, 198)
+        Me.txt_mlsp.Name = "txt_mlsp"
+        Me.txt_mlsp.Size = New System.Drawing.Size(203, 20)
+        Me.txt_mlsp.TabIndex = 11
+        '
+        'txt_ctsp
+        '
+        Me.txt_ctsp.Location = New System.Drawing.Point(131, 162)
+        Me.txt_ctsp.Name = "txt_ctsp"
+        Me.txt_ctsp.Size = New System.Drawing.Size(203, 20)
+        Me.txt_ctsp.TabIndex = 11
+        '
+        'txt_sl
+        '
+        Me.txt_sl.Location = New System.Drawing.Point(132, 130)
+        Me.txt_sl.Name = "txt_sl"
+        Me.txt_sl.Size = New System.Drawing.Size(202, 20)
+        Me.txt_sl.TabIndex = 11
+        '
+        'txt_dg
+        '
+        Me.txt_dg.Location = New System.Drawing.Point(132, 91)
+        Me.txt_dg.Name = "txt_dg"
+        Me.txt_dg.Size = New System.Drawing.Size(202, 20)
+        Me.txt_dg.TabIndex = 10
+        '
+        'txt_msp
+        '
+        Me.txt_msp.Location = New System.Drawing.Point(131, 19)
+        Me.txt_msp.Name = "txt_msp"
+        Me.txt_msp.Size = New System.Drawing.Size(203, 20)
+        Me.txt_msp.TabIndex = 9
+        '
+        'txt_tsp
+        '
+        Me.txt_tsp.Location = New System.Drawing.Point(133, 57)
+        Me.txt_tsp.Name = "txt_tsp"
+        Me.txt_tsp.Size = New System.Drawing.Size(201, 20)
+        Me.txt_tsp.TabIndex = 8
+        '
+        'btn_thoat
+        '
+        Me.btn_thoat.Location = New System.Drawing.Point(467, 235)
+        Me.btn_thoat.Name = "btn_thoat"
+        Me.btn_thoat.Size = New System.Drawing.Size(75, 23)
+        Me.btn_thoat.TabIndex = 7
+        Me.btn_thoat.Text = "Thoát"
+        Me.btn_thoat.UseVisualStyleBackColor = True
+        '
+        'btn_xoa
+        '
+        Me.btn_xoa.Location = New System.Drawing.Point(353, 235)
+        Me.btn_xoa.Name = "btn_xoa"
+        Me.btn_xoa.Size = New System.Drawing.Size(75, 23)
+        Me.btn_xoa.TabIndex = 6
+        Me.btn_xoa.Text = "Xóa"
+        Me.btn_xoa.UseVisualStyleBackColor = True
+        '
+        'btn_sua
+        '
+        Me.btn_sua.Location = New System.Drawing.Point(241, 235)
+        Me.btn_sua.Name = "btn_sua"
+        Me.btn_sua.Size = New System.Drawing.Size(75, 23)
+        Me.btn_sua.TabIndex = 5
+        Me.btn_sua.Text = "Sữa"
+        Me.btn_sua.UseVisualStyleBackColor = True
+        '
+        'btn_them
+        '
+        Me.btn_them.Location = New System.Drawing.Point(131, 235)
+        Me.btn_them.Name = "btn_them"
+        Me.btn_them.Size = New System.Drawing.Size(75, 23)
+        Me.btn_them.TabIndex = 4
+        Me.btn_them.Text = "Thêm"
+        Me.btn_them.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(28, 201)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(90, 13)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "Mã loại sản phẩm"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(28, 169)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(88, 13)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Chi tiết sản phẩm"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 133)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Số lượng"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 98)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Đơn giá"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Tên sản phẩm "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Mã sản phẩm"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.dgv_dssp)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 294)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(602, 178)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Danh sách sản phẩm"
+        '
+        'dgv_dssp
+        '
+        Me.dgv_dssp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_dssp.Location = New System.Drawing.Point(6, 17)
+        Me.dgv_dssp.Name = "dgv_dssp"
+        Me.dgv_dssp.Size = New System.Drawing.Size(590, 150)
+        Me.dgv_dssp.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.asm.My.Resources.Resources.xehoi6
+        Me.PictureBox1.Location = New System.Drawing.Point(353, 19)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(239, 199)
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
+        'frm_qlsp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(625, 484)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frm_qlsp"
+        Me.Text = "QUẢN LÍ SẢN PHẨM"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.dgv_dssp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents txt_sl As System.Windows.Forms.TextBox
+    Friend WithEvents txt_dg As System.Windows.Forms.TextBox
+    Friend WithEvents txt_msp As System.Windows.Forms.TextBox
+    Friend WithEvents txt_tsp As System.Windows.Forms.TextBox
+    Friend WithEvents btn_thoat As System.Windows.Forms.Button
+    Friend WithEvents btn_xoa As System.Windows.Forms.Button
+    Friend WithEvents btn_sua As System.Windows.Forms.Button
+    Friend WithEvents btn_them As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txt_mlsp As System.Windows.Forms.TextBox
+    Friend WithEvents txt_ctsp As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents dgv_dssp As System.Windows.Forms.DataGridView
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+End Class
